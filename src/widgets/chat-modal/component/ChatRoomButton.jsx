32003e2cbd358d19b10240d9489997ce6d7309dc
dookh99 <dookh1999@gmail.com>
2026-05-29 11:26:50 +0900

    feat: 채팅 모달 활성화 시 뒷 배경 잠금처리. 채팅방, 채팅 메시지 스크롤. 선택한 채팅방 강조.

--- src/widgets/chat-modal/component/ChatRoomButton.jsx
+++ src/widgets/chat-modal/component/ChatRoomButton.jsx
@@ -1,15 +1,27 @@
 
 
-const ChatRoomButton = ({ myChatRoomData, getCurrentChatRoom }) => {
+const ChatRoomButton = ({ myChatRoomData, getCurrentChatRoom, isSelected, setSelectedChatRoomId }) => {
 
     return (
         <>
-            <button className="chat-modal-partner" type="button" onClick={() => getCurrentChatRoom(myChatRoomData)}>
+            <button className="chat-modal-partner" type="button"
+                    style={isSelected ? {backgroundColor:"#f0f5ff"} : {backgroundColor:"white"}}
+                    onClick={() => {
+                        getCurrentChatRoom(myChatRoomData);
+                        if(isSelected === false) {
+                            setSelectedChatRoomId(myChatRoomData?.chatRoomId);
+                        } else {
+                            setSelectedChatRoomId(0);
+                        }
+                    }
+            }>
                 <img src={myChatRoomData.contactUserInfo?.profileImageUrl} alt=""/>
                 <span className="chat-modal-partner-copy">
                 <strong>{myChatRoomData.contactUserInfo?.name}</strong>
-                <span>{myChatRoomData.lastMessageInfo?.content}</span>
-            </span>
+                    {myChatRoomData.lastMessageInfo?.content.length > 22
+                    ? (<span>{myChatRoomData.lastMessageInfo?.content.substring(0,22) + "..."}</span>)
+                    : (<span>{myChatRoomData.lastMessageInfo?.content}</span>)}
+                </span>
                 <time>{myChatRoomData.lastMessageAtConvert}</time>
                 { myChatRoomData.unReadMessageCount <= 0 ?
                     <span className="chat-room-badge" style={{visibility:'hidden'}}>{myChatRoomData.unReadMessageCount}</span> :
