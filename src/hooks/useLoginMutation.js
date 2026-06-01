bba333b9621c8c02ec05536d2485d8d6b20d07e5
dookh99 <dookh1999@gmail.com>
2026-06-01 11:39:39 +0900

    feat: 채팅 텍스트, 이미지 전송 추가. 로그인 시 Storage에 user정보 넣기.

--- src/hooks/useLoginMutation.js
+++ src/hooks/useLoginMutation.js
@@ -2,6 +2,7 @@ import { useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { loginApi } from "../api/authApi";
 import { useAuthStore } from "../store/authStore";
+import {apiClient} from "../api/apiClient.js";
 
 export const useLoginMutation = () => {
     const navigate = useNavigate();
@@ -17,14 +18,14 @@ export const useLoginMutation = () => {
 
 
         // 2. 로그인 성공 시 실행될 콜백 함수
-        onSuccess: (data,{ keepLogin }) => {
+        onSuccess: async (data,{ keepLogin }) => {
             // 스토어와 로컬스토리지에 토큰 및 유저 정보 자동 저장
             login({ ...data, keepLogin });
             try {
                 // ② [디펜시브/추천] 로그인 직후, 방금 받은 토큰을 들고 내 프로필 정보 채우기
                 // (백엔드에 회원 정보 조회 엔드포인트가 구현되어 있다면 주석을 해제하세요)
-                // const userResponse = await apiClient.get("/members/me");
-                // setUser(userResponse.data);
+                const userResponse = await apiClient.get("/mypage/me");
+                setUser(userResponse.data.data);
             } catch (userError) {
                 console.error("유저 정보를 불러오는데 실패했습니다:", userError);
             }
